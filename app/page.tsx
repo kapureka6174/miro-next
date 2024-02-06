bb5c09aa7d973ecb80d7de7f2a4881559585e687
kapureka6174 <mitsu.n.0109@gmail.com>
2024-02-07 07:05:43 +0900

    install convex and clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return <Button>Click Me!</Button>;
+  return (
+    <div className='flex flex-col gap-y-4'>
+      <div>会員限定ページ</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
